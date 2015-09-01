d8cfd0ffd23d508ac80d7f015de980fa5415806f
Abraham Haskins <abeisgreat@abeisgreat.com>
2015-09-01 16:13:03 -0500

    Adds some comments

--- src/js/app.js
+++ src/js/app.js
@@ -8,9 +8,12 @@ var UI = require('ui');
 var Vector2 = require('vector2');
 require('./firebase');
 
-var ref = new Firebase(YOUR_FIREBASE);
+// Create a new Firebase reference
+var ref = new Firebase("bucket.firebaseio.com/pebble");
+
 
 function showMenu() {
+  // We create a simple menu with a few options
   var menu = new UI.Menu({
     sections: [{
       items: [{
@@ -32,30 +35,37 @@ function showMenu() {
     }]
   });
 
+  // When the user selects an option...
   menu.on('select', function(e) {
+    // We use the title to perform operations
     switch (e.item.title) {
       case "View Data":
+        // Show the data view window
         showData();
         break;
       case "Set a value":
+        // Set a location in Firebase
         ref.child('set/value').set(true);
         break;
       case "Push a value":
+        // Push the current time into a firebase "array"
         ref.child("push").push((new Date().toString()));
         break;
       case "Delete data":
+        // ... or remove all the data at the location
         ref.remove();
         break;
     }
-    console.log('Selected item: ' + e.section + ' ' + e.item);
   });
 
+  // Finally make sure the menu is set to show.
   menu.show();
 }
 
 function showData() {
   // Create a new Pebble window
   var wind = new UI.Window();
+
   // Then create a textfield which will be updated by Firebase.
   var textfield = new UI.Text({
     position: new Vector2(0, 50),
@@ -64,23 +74,38 @@ function showData() {
     text: "Loading...",
     textAlign: 'center'
   });
+
+  // Add the textfield to our window object
   wind.add(textfield);
 
-  // Pull up the most recent time pushed into our Firebase ref.
-  ref
-    .child('push')
-    .limitToLast(1)
-    .on('child_added', function (datetime) {
-      var text = datetime.val() || "No data!";
-      textfield.text(text);
-      wind.show();
-    });
+  // Create a new Firebase ref which points to the last child of /push
+  var recentPushRef = ref
+      .child('push')
+      .limitToLast(1);
 
+  // Pull up the most recent time pushed into our Firebase ref.
+  recentPushRef.on('child_added', function (datetime) {
+    var text = datetime.val();
+    textfield.text(text);
+    wind.show();
+  });
+
+  /*
+    We use a once event to determine if location exists
+    i.e. has any data been written
+  */
+  recentPushRef.once('value', function (snapshot) {
+    if (!snapshot.exists())
+      textfield.text("No data, yet!");
+
+    wind.show();
+  });
+
+  /*
+  Make sure we're only getting updates from
+  Firebase when the card is displayed, so we .off when the window hides
+  */
   wind.on('hide', function () {
-    /*
-    Make sure we're only getting updates from
-    Firebase when the card is displayed.
-    */
     ref.child('push').off('child_added');
   });
 }
